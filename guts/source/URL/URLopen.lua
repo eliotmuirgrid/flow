7bffc2cfcb4f7baeb7d2cbff0cec89896fd17263
Eliot Muir <eliot.muir@interfaceware.com>
2026-07-30 13:08:26 -0500

    Update

--- guts/source/URL/URLopen.lua
+++ guts/source/URL/URLopen.lua
@@ -1,11 +1,15 @@
 function URLopen(Url)
    COL_VAR(Url)
-   local C
-   if     (OSisMac())     then C='open "'..Url..'"' 
-   elseif (OSisWindows()) then C='start '..Url      else
+   local Windows = 'start "'..Url..'"'
+   local Mac     = 'open "'..Url..'"'
+
+   if (OSisMac()) then 
+      os.execute(Mac);	
+   elseif (OSisWindows()) then
+      os.execute(Windows);
+   else
       error("Sorry URLopen doesn't support your operating system yet.");
    end
    print "Eliot suggests be careful about using URLopen with any URLs you didn't create yourself"
    print "This code is unsafe - but a lot of engineering effort will be required to make it safe.  Later!"
-   os.execute(C);
 end
